4990613379e35c950b8342308d6106c237b7cb91
RachidNaitSaid2002 <rachidnaitsaid15@gmail.com>
2026-01-21 09:49:42 +0100

    Setup airflow

---
+++ airflow/init_airflow.sh
@@ -0,0 +1,9 @@
+airflow db init
+
+airflow users create \
+    --username admin \
+    --password admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@admin.com
